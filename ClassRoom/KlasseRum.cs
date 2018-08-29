77b395986c2116daefefba4f85055a5a8f4e5f5c
andersbor <anbo@easj.dk>
2018-08-29 13:57:42 +0200

    ny klasse Klasserum

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassRoom
+{
+    internal class KlasseRum
+    {
+        public string KlasseNavn { get; set; }
+        public List<Student> Klasseliste { get; set; }
+
+        public DateTime SemesterStart { get; set; }
+    }
+}
